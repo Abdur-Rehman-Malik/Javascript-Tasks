067ee3e51d87bd9c725e53819e053e55a388b49f
Abdur-Rehman-Malik <abdurrehmanm849@gmail.com>
2024-05-13 03:05:59 +0500

    promise

--- promise.js
+++ promise.js
@@ -1,10 +1,10 @@
 // setTimeout(() =>{
-//     console.log(" Line 1")
+//    console.log(" Line 1")
 // });
 
 
 // function display () {
-// \    console.log("line  2")
+//     console.log("line  2")
 // }
 
 // display();
@@ -39,36 +39,36 @@
 //     console.log(value2);
 // })
 
-// const promiseOne = new Promise((res) => {
-//     setTimeout(() => {
-//         res("Line 1")
-//     },5000)
-// })
+const promiseOne = new Promise((res) => {
+    setTimeout(() => {
+        res("Line 1")
+    },1000)
+})
 
-// const promiseTwo = new Promise((res) => {
-//     res("Line 2");
-// })
+const promiseTwo = new Promise((res) => {
+    res("Line 2");
+})
 
-// promiseOne.then((value) => {
-//     console.log(value);
-// })
-// promiseTwo.then((value) => {
-//     console.log(value);
-// })
+promiseOne.then((value) => {
+    console.log(value);
+})
+promiseTwo.then((value) => {
+    console.log(value);
+})
 
-// console.log("async");
+console.log("async");
 
-// const main = async() =>{
-//     await promiseOne.then((value) => {
-//         console.log(value)
+const main = async() =>{
+    await promiseOne.then((value) => {
+        console.log(value)
 
-//     })
+    })
 
-//    await promiseTwo.then((value) => {
-//         console.log(value);
-//     })
-// }
-// main();
+   await promiseTwo.then((value) => {
+        console.log(value);
+    })
+}
+main();
 
 // const greet = (name,age) => {
 //     console.log(`i am ${name} and my age is ${age}`);
@@ -81,18 +81,18 @@
 // let c=30;
 // console.log("updated value is:" , (c,b,a))
 
-function myfun(arry){
-    arry[0] = 10;
-}
+// function myfun(arry){
+//     arry[0] = 10;
+// }
 
-let arry=["1","2","3"]
-myfun(arry);
-
-
-console.log(arry)
+// let arry=["1","2","3"]
+// myfun(arry);
 
 
-const arr = [1,2,3,4]
-for (let i=0; i<arr.length; i++)
-        arr[i]+=2;
-console.log (arr)
+// console.log(arry)
+
+
+// const arr = [1,2,3,4]
+// for (let i=0; i<arr.length; i++)
+//         arr[i]+=2;
+// console.log (arr)
